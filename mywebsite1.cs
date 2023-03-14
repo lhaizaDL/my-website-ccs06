61c58a4a09fbdfbbb80246bc5e60417b77c9c8c9
lhaiza claire g. de leon <deleon.lhiazaclaire@auf.edu.ph>
2023-03-14 17:50:49 +0800

    My website initial files

---
+++ mywebsite1.cs
@@ -0,0 +1,140 @@
+<!DOCTYPE html><html><head><link href="https://fonts.googleapis.com/css?family=Familjen+Grotesk&display=swap" rel="stylesheet" /></head><body><div class="v1_16"><div class="v1_2"></div><div class="v1_3"></div><div class="v1_4"></div><span class="v1_5">Lhaiza Claire</span><div class="v1_6"></div><div class="v1_8"></div><div class="v1_7"></div><span class="v1_9">Home</span><span class="v1_11">Contact</span><span class="v1_13">about me</span><span class="v1_14">“Setting goals is the first step in turning the invisible into the visible.”</span></div></body></html> <br/><br/> <style>* {
+  box-sizing: border-box;
+}
+body {
+  font-size: 14px;
+}
+.v1_16 {
+  width: 824px;
+  height: 480px;
+  background: rgba(255,255,255,1);
+  opacity: 1;
+  position: relative;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v1_2 {
+  width: 771px;
+  height: 450px;
+  background: rgba(217,217,217,1);
+  opacity: 1;
+  position: absolute;
+  top: 16px;
+  left: 33px;
+  overflow: hidden;
+}
+.v1_3 {
+  width: 771px;
+  height: 78px;
+  background: rgba(246,238,227,1);
+  opacity: 1;
+  position: absolute;
+  top: 16px;
+  left: 33px;
+  overflow: hidden;
+}
+.v1_4 {
+  width: 771px;
+  height: 372px;
+  background: url("../images/v1_4.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 94px;
+  left: 33px;
+  overflow: hidden;
+}
+.v1_5 {
+  width: 373px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 24px;
+  left: 69px;
+  font-family: Familjen Grotesk;
+  font-weight: Regular;
+  font-size: 48px;
+  opacity: 1;
+  text-align: left;
+}
+.v1_6 {
+  width: 92px;
+  height: 28px;
+  background: rgba(217,217,217,1);
+  opacity: 1;
+  position: absolute;
+  top: 45px;
+  left: 367px;
+  overflow: hidden;
+}
+.v1_8 {
+  width: 92px;
+  height: 28px;
+  background: rgba(217,217,217,1);
+  opacity: 1;
+  position: absolute;
+  top: 45px;
+  left: 122px;
+  overflow: hidden;
+}
+.v1_7 {
+  width: 109px;
+  height: 28px;
+  background: rgba(217,217,217,1);
+  opacity: 1;
+  position: absolute;
+  top: 45px;
+  left: 253px;
+  overflow: hidden;
+}
+.v1_9 {
+  width: 92px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 45px;
+  left: 368px;
+  font-family: Familjen Grotesk;
+  font-weight: Regular;
+  font-size: 20px;
+  opacity: 1;
+  text-align: center;
+}
+.v1_11 {
+  width: 92px;
+  color: rgba(192,145,117,1);
+  position: absolute;
+  top: 45px;
+  left: 121px;
+  font-family: Familjen Grotesk;
+  font-weight: Regular;
+  font-size: 20px;
+  opacity: 1;
+  text-align: center;
+}
+.v1_13 {
+  width: 109px;
+  color: rgba(192,145,117,1);
+  position: absolute;
+  top: 45px;
+  left: 253px;
+  font-family: Familjen Grotesk;
+  font-weight: Regular;
+  font-size: 20px;
+  opacity: 1;
+  text-align: center;
+}
+.v1_14 {
+  width: 488px;
+  color: rgba(192,145,117,1);
+  position: absolute;
+  top: 154px;
+  left: 207px;
+  font-family: Familjen Grotesk;
+  font-weight: Regular;
+  font-size: 48px;
+  opacity: 1;
+  text-align: center;
+}
+</style>
